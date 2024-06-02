5d0009ca1cce85e87e04ea6ffe883119e054071b
citrus327 <citrus327@outlook.com>
2024-06-02 16:52:27 +0800

    feat: tailwind

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,9 @@
+console.log("1231231");
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
