f85db6d4d9f7bc1ef6cfab1afcba15cb962e233e
citrus327 <citrus327@outlook.com>
2024-06-03 00:39:32 +0800

    feat: clear

--- backend/prisma/migrations/20240602081058_init/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "user" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "name" TEXT NOT NULL
-);
-
--- CreateIndex
-CREATE UNIQUE INDEX "user_name_key" ON "user"("name");
