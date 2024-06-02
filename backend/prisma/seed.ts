7a3a8b6cbd4ac3742799a333d93e5d01fcfc0d21
citrus327 <citrus327@outlook.com>
2024-06-02 16:39:35 +0800

    feat: db structure designs

--- backend/prisma/seed.ts
+++ backend/prisma/seed.ts
@@ -1,11 +1,13 @@
 import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
+
 async function main() {
   const userA = await prisma.user.upsert({
     where: { name: "A" },
     update: {},
     create: {
       name: "A",
+      role: "Sender",
     },
   });
   const userB = await prisma.user.upsert({
@@ -13,6 +15,7 @@ async function main() {
     update: {},
     create: {
       name: "B",
+      role: "Receiver",
     },
   });
   console.log({ userA, userB });
